64f893b8e2c52c5e0b783e5e7a778b9e16866536
TeddyMuli <tmuli974@gmail.com>
2024-12-09 10:06:28 +0300

    created RedisClient class

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import { createClient, print } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.log('Redis client not connected to the server:', err.toString());
+    });    
+  }
+
+  isAlive() {
+    this.client.on('connect', () => {
+      return true;
+    });
+  }
+
+  async get(key) {
+    this.client.get(key, (_err, reply) => {
+      console.log(reply.toString());
+    });
+  }
+
+  async set(key, duration) {
+    this.client.set(key, duration, print);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
